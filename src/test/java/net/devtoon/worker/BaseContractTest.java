1b79b0cc399c2b7a1c1469200993a607289d951b
Dominik Steinbinder <domi.github@devtoon.net>
2018-12-08 17:01:33 +0100

    rename base contract test class

--- src/test/java/net/devtoon/worker/BaseContractTest.java
+++ src/test/java/net/devtoon/worker/BaseContractTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = WorkerApplication.class)
-public abstract class BaseClass {
+public abstract class BaseContractTest {
 
     @Autowired
     private WorkerController controller;
